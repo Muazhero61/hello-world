b8e75d883854db382a394bfe7e44c346248cc678
Muazhero61 <68938857+Muazhero61@users.noreply.github.com>
2021-02-15 09:27:32 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<?php
+	  echo nl2br("Hello World! \n");
+
+	  echo nl2br("PHP is so easy!\n");
+
+	  echo nl2br("takes time to master it\n");
+	?>
+
+	<!-- creted by Muhd Muaz -->
+
+</body>
+</html>
